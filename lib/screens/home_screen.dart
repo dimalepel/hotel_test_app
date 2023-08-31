5e196565c06deb4f78329bd6a935cecf758f0076
Dimitry Vashkevich <dimalepel@mail.ru>
2023-08-31 18:48:40 +0300

    feat: add home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:banner_carousel/banner_carousel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:hotel_test_app/widgets/chip_item.dart';
 
 import '../themes/app_colors.dart';
 import '../widgets/slide_card.dart';
@@ -11,7 +12,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.greyBg,
+      backgroundColor: AppColors.greyAlt,
       appBar: AppBar(
         title: Text(
           'Отель',
@@ -28,6 +29,8 @@ class HomeScreen extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Container(
               padding: const EdgeInsets.only(
@@ -91,11 +94,293 @@ class HomeScreen extends StatelessWidget {
                         ),
                       ],
                     ),
+                  ),
+                  SizedBox(height: 8,),
+                  Text(
+                    'Steigenberger Makadi',
+                    style: TextStyle(
+                      fontFamily: 'San Francisco',
+                      fontSize: 22,
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.black
+                    ),
+                  ),
+                  SizedBox(height: 8,),
+                  TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      minimumSize: Size(0, 0)
+                    ),
+                    child: Text(
+                      'Madinat Makadi, Safaga Road, Makadi Bay, Египет',
+                      style: TextStyle(
+                          fontFamily: 'San Francisco',
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          color: AppColors.blue
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16,),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        'от 134 673 ₽',
+                        style: TextStyle(
+                          fontFamily: 'San Francisco',
+                          fontSize: 30,
+                          fontWeight: FontWeight.w600,
+                          color: AppColors.black
+                        ),
+                      ),
+                      SizedBox(width: 8,),
+                      Text(
+                        'за тур с перелётом',
+                        style: TextStyle(
+                            fontFamily: 'San Francisco',
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                            color: AppColors.greyDark
+                        ),
+                      ),
+                    ],
                   )
                 ],
               ),
-            )
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: AppColors.white,
+              ),
+              padding: EdgeInsets.all(16),
+              margin: EdgeInsets.only(bottom: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Об отеле',
+                    style: TextStyle(
+                      fontFamily: 'San Francisco',
+                      fontSize: 22,
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.black
+                    ),
+                  ),
+                  SizedBox(height: 16,),
+                  Wrap(
+                    spacing: 8,
+                    runSpacing: 8,
+                    children: [
+                      ChipItem(label: '3-я линия'),
+                      ChipItem(label: 'Платный Wi-Fi в фойе'),
+                      ChipItem(label: '30 км до аэропорта'),
+                      ChipItem(label: '1 км до пляжа'),
+                    ],
+                  ),
+                  SizedBox(height: 12,),
+                  Text(
+                    'Отель VIP-класса с собственными гольф полями. Высокий уровнь сервиса. Рекомендуем для респектабельного отдыха. Отель принимает гостей от 18 лет!',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'San Francisco',
+                      fontWeight: FontWeight.w400
+                    ),
+                  ),
+                  SizedBox(height: 16,),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: AppColors.grey,
+                    ),
+                    padding: EdgeInsets.all(15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            SvgPicture.asset(
+                              'assets/images/svgs/icon_smile.svg',
+                              width: 24,
+                              height: 24,
+                            ),
+                            SizedBox(width: 12,),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Удобства',
+                                  style: TextStyle(
+                                    fontFamily: 'San Francisco',
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500,
+                                    color: AppColors.blackAlt
+                                  ),
+                                ),
+                                SizedBox(height: 2,),
+                                Text(
+                                  'Самое необходимое',
+                                  style: TextStyle(
+                                      fontFamily: 'San Francisco',
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w500,
+                                      color: AppColors.greyDark
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Spacer(),
+                            SvgPicture.asset(
+                              'assets/images/svgs/icon_chewron_right.svg',
+                              width: 24,
+                              height: 24,
+                            ),
+                          ],
+                        ),
+                        Container(
+                          margin: EdgeInsets.symmetric(vertical: 10),
+                          height: 1,
+                          width: MediaQuery.of(context).size.width - 96,
+                          color: AppColors.greyDark15,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            SvgPicture.asset(
+                              'assets/images/svgs/icon_check.svg',
+                              width: 24,
+                              height: 24,
+                            ),
+                            SizedBox(width: 12,),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Что включено',
+                                  style: TextStyle(
+                                      fontFamily: 'San Francisco',
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500,
+                                      color: AppColors.blackAlt
+                                  ),
+                                ),
+                                SizedBox(height: 2,),
+                                Text(
+                                  'Самое необходимое',
+                                  style: TextStyle(
+                                      fontFamily: 'San Francisco',
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w500,
+                                      color: AppColors.greyDark
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Spacer(),
+                            SvgPicture.asset(
+                              'assets/images/svgs/icon_chewron_right.svg',
+                              width: 24,
+                              height: 24,
+                            ),
+                          ],
+                        ),
+                        Container(
+                          margin: EdgeInsets.symmetric(vertical: 10),
+                          height: 1,
+                          width: MediaQuery.of(context).size.width - 96,
+                          color: AppColors.greyDark15,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            SvgPicture.asset(
+                              'assets/images/svgs/icon_close.svg',
+                              width: 24,
+                              height: 24,
+                            ),
+                            SizedBox(width: 12,),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Что не включено',
+                                  style: TextStyle(
+                                      fontFamily: 'San Francisco',
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500,
+                                      color: AppColors.blackAlt
+                                  ),
+                                ),
+                                SizedBox(height: 2,),
+                                Text(
+                                  'Самое необходимое',
+                                  style: TextStyle(
+                                      fontFamily: 'San Francisco',
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w500,
+                                      color: AppColors.greyDark
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Spacer(),
+                            SvgPicture.asset(
+                              'assets/images/svgs/icon_chewron_right.svg',
+                              width: 24,
+                              height: 24,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
           ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        height: 72,
+        padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+        decoration: BoxDecoration(
+          color: AppColors.white,
+          border: Border(
+            top: BorderSide(
+              color: AppColors.greyLight,
+              width: 1
+            )
+          )
+        ),
+        child: InkWell(
+          onTap: () {},
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: AppColors.blue
+            ),
+            child: Center(
+              child: Text(
+                'К выбору номера',
+                style: TextStyle(
+                  fontFamily: 'San Francisco',
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: AppColors.white
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
--- lib/themes/app_colors.dart
+++ lib/themes/app_colors.dart
@@ -9,8 +9,9 @@ abstract class AppColors {
   static Color blue = const Color(0xff0d72ff);
   static Color blue10 = const Color(0xff0d72ff).withOpacity(0.1);
   static Color greyDark = const Color(0xff828796);
+  static Color greyDark15 = const Color(0xff828796).withOpacity(0.15);
   static Color grey = const Color(0xfffbfbfc);
+  static Color greyLight = const Color(0xffe8e9ec);
   static Color greyAlt = const Color(0xfff6f6f9);
   static Color blackAlt = const Color(0xff2c3035);
-  static Color greyBg = const Color(0xffd8d8d8);
 }
