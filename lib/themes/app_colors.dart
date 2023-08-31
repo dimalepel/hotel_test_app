59811d9de6ba33c8e8c55a22fb155e24e88e3163
Dimitry Vashkevich <dimalepel@mail.ru>
2023-08-31 14:28:49 +0300

    feat: add app colors

---
+++ lib/themes/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static Color transparent = Colors.transparent;
+  static Color black = Colors.black;
+  static Color white = Colors.white;
+  static Color orange = const Color(0xffffa800);
+  static Color orange20 = const Color(0xffFFC700).withOpacity(0.2);
+  static Color blue = const Color(0xff0d72ff);
+  static Color blue10 = const Color(0xff0d72ff).withOpacity(0.1);
+  static Color greyDark = const Color(0xff828796);
+  static Color grey = const Color(0xfffbfbfc);
+  static Color greyAlt = const Color(0xfff6f6f9);
+  static Color blackAlt = const Color(0xff2c3035);
+}
