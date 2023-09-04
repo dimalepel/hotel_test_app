e72950f78d30597cb44575b1123061afd35e14e5
Dimitry Vashkevich <dimalepel@mail.ru>
2023-09-04 11:11:48 +0300

    feat: add hotel provider

--- lib/providers/hotel_provider.dart
+++ lib/providers/hotel_provider.dart
@@ -4,5 +4,15 @@ import 'package:hotel_test_app/services/hotel_service.dart';
 import '../models/hotel_model.dart';
 
 class HotelDataProvider extends ChangeNotifier {
+  Hotel? hotel;
+  bool loading = false;
+  final hotelService = HotelService();
 
+  getHotelData() async {
+    loading = true;
+    hotel = (await hotelService.getSingleHotel())!;
+    loading = false;
+
+    notifyListeners();
+  }
 }
